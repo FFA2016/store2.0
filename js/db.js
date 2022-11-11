76ca53ec76743c3705c79a4767c1fa7329ec630f
FFA2016 <minecraftffaa@gmail.com>
2022-11-11 11:46:32 +0100

    - Added first Backend implementations - Added new Shirt

---
+++ js/db.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+//import mysql from 'mysql';
+
+function connecttodatabase(string) {
+    var con = mysql.createConnection({
+
+        host: "185.236.11.136",
+        user: "management",
+        password: "9Gy6@a79x"
+
+    });
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
+    con.query('SELECT * FROM shop.produkte AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0]);
+    });
+
+};
